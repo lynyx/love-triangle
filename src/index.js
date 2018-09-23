371bac2162c136aaf96d6ac582e64f4c2c2feaf7
kanstantsin-laurenka <kanstantsin.laurenka@kufar.by>
2018-09-23 14:12:25 +0300

    Love triangle solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let counter = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    const firstSpichonee = preferences[i] - 1;
+    const secondSpichonee = preferences[firstSpichonee] - 1;
+    const thirdSpichonee = preferences[secondSpichonee] - 1;
+
+    if (firstSpichonee === i || firstSpichonee === secondSpichonee || thirdSpichonee === secondSpichonee) continue;
+
+    if (thirdSpichonee === i) {
+      counter++;
+    }
+  }
+
+  return counter / 3;
 };
